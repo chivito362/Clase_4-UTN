f97360398d5c5f2820602e4f0e7a0ea495ead684
chivito362 <chivito362@gmail.com>
2023-12-01 17:17:15 -0300

    cambios

--- src/main/java/com/Rastas/Main.java
+++ src/main/java/com/Rastas/Main.java
@@ -6,8 +6,8 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         //para probar la prueba 5 agregar cambiar el LocalDateTime.now() por LocalDateTime.now().minusHours(24) que setea el tiempo para que transcurran 24 horas desde la ultima reproduccion
-        //Cancion cancion=new Cancion("The Scientist","“Coldplay”","A Rush of Blood to the head", 2002,999,0,4999, LocalDateTime.now());
-        
+        Cancion cancion=new Cancion("The Scientist","“Coldplay”","A Rush of Blood to the head", 2002,999,0,4999, LocalDateTime.now());
+        cancion.mostrarMensaje();
         
     }
 }
